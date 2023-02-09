356816294a251bd19b380d3348faeefb993e21e0
Yutao He <yutaohe@uchicago.edu>
2023-02-08 20:21:28 -0600

    Revised code of Data Cleaning

--- DataCleaning.py
+++ DataCleaning.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 
 def clean_data(filename):
     dataset = pd.read_csv(filename,header=None)
@@ -10,5 +11,16 @@ def clean_data(filename):
     dataset.insert(4,"Year",year)
     dataset.to_csv(filename,header=False, index=False)
 
-clean_data("AmericanSecretaryVisit.csv")
-clean_data("AmericanPresidentVisit.csv")
+def year_sort(filename):
+    dataset = pd.read_csv(filename, header=None)
+    sorted_year = []
+    for i in range (dataset.shape[0]):
+        year_new = dataset.iloc[i,4]
+        year_new = math.ceil(year_new/5)*5
+        sorted_year.append(year_new)
+    sorted_year = pd.DataFrame(sorted_year)
+    dataset.insert(5,"Sorted_Year",sorted_year)
+    dataset.to_csv(filename,header=False, index=False)
+
+year_sort("AmericanSecretaryVisit.csv")
+year_sort("AmericanPresidentVisit.csv")
